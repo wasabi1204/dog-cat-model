acab44bbf1057be649fa42da98bb4cb63e6c44fc
Iizawa <Iizawa@my.mac.local>
2024-07-20 09:22:21 +0900

    'main'

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,26 +1,45 @@
-// Description:
-//   Utility commands surrounding Hubot uptime.
-//
-// Commands:
-//   ping - Reply with pong
-//   echo <text> - Reply back with <text>
-//   time - Reply with current time
-'use strict';
+const Clarifai = require('clarifai');
+const axios = require('axios');
+
+const app = new Clarifai.App({
+    apiKey: '5654b2812fd44f61a85e7543a89faa27'  // ClarifaiのAPIキーを入力
+});
 
 module.exports = (robot) => {
-  robot.respond(/PING$/i, (res) => {
-    res.send('PONG');
-  });
+    robot.respond(/(.+)$/, (res) => {
+        const message = res.message;
 
-  robot.respond(/ADAPTER$/i, (res) => {
-    res.send(robot.adapterName);
-  });
+        if (message.attachments && message.attachments.length > 0 && message.attachments[0].type === 'image') {
+            const imageUrl = message.attachments[0].url;
 
-  robot.respond(/ECHO (.*)$/i, (res) => {
-    res.send(res.match[1]);
-  });
+            // 画像のダウンロード処理
+            const downloadImage = async (url) => {
+                const response = await axios.get(url, { responseType: 'arraybuffer' });
+                return Buffer.from(response.data, 'binary').toString('base64');
+            };
 
-  robot.respond(/TIME$/i, (res) => {
-    res.send(`Server time is: ${new Date()}`);
-  });
+            downloadImage(imageUrl).then(imageBytes => {
+                app.models.predict("dog-catmodel", { base64: imageBytes }).then(
+                    function (response) {
+                        const concepts = response.outputs[0].data.concepts;
+                        if (concepts.length > 0) {
+                            const topConcept = concepts[0].name;
+                            res.send(`これは${topConcept}の画像ですね。`);
+                        } else {
+                            res.send('画像の内容を認識できませんでした。');
+                        }
+                    },
+                    function (err) {
+                        console.error(err);
+                        res.send('画像の解析に失敗しました。');
+                    }
+                );
+            }).catch(err => {
+                console.log("Error: " + err);
+                res.send('画像のダウンロードに失敗しました。');
+            });
+        } else {
+            res.send('画像を送信してください。');
+        }
+    });
 };
