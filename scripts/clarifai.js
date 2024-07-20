fc59fa71dd3d89940ac385c7197e6b18e9926954
Iizawa <Iizawa@my.mac.local>
2024-07-20 09:25:44 +0900

    'main'

--- scripts/clarifai.js
+++ scripts/clarifai.js
@@ -6,38 +6,31 @@ const app = new Clarifai.App({
 });
 
 module.exports = (robot) => {
-    robot.respond(/(.+)$/, (res) => {
+    robot.respond(/(.+)$/, async (res) => {
         const message = res.message;
-
+        
         if (message.attachments && message.attachments.length > 0 && message.attachments[0].type === 'image') {
             const imageUrl = message.attachments[0].url;
-
-            // 画像のダウンロード処理
-            const downloadImage = async (url) => {
-                const response = await axios.get(url, { responseType: 'arraybuffer' });
-                return Buffer.from(response.data, 'binary').toString('base64');
-            };
-
-            downloadImage(imageUrl).then(imageBytes => {
-                app.models.predict("dog-catmodel", { base64: imageBytes }).then(
-                    function (response) {
-                        const concepts = response.outputs[0].data.concepts;
-                        if (concepts.length > 0) {
-                            const topConcept = concepts[0].name;
-                            res.send(`これは${topConcept}の画像ですね。`);
-                        } else {
-                            res.send('画像の内容を認識できませんでした。');
-                        }
-                    },
-                    function (err) {
-                        console.error(err);
-                        res.send('画像の解析に失敗しました。');
-                    }
-                );
-            }).catch(err => {
-                console.log("Error: " + err);
-                res.send('画像のダウンロードに失敗しました。');
-            });
+            
+            try {
+                // 画像のダウンロード処理
+                const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+                const imageBytes = Buffer.from(response.data, 'binary').toString('base64');
+                
+                // 画像をClarifai APIで解析
+                const clarifaiResponse = await app.models.predict("dog-catmodel", { base64: imageBytes });
+                const concepts = clarifaiResponse.outputs[0].data.concepts;
+                
+                if (concepts.length > 0) {
+                    const topConcept = concepts[0].name;
+                    res.send(`これは${topConcept}の画像ですね。`);
+                } else {
+                    res.send('画像の内容を認識できませんでした。');
+                }
+            } catch (err) {
+                console.error(err);
+                res.send('画像の解析に失敗しました。');
+            }
         } else {
             res.send('画像を送信してください。');
         }
